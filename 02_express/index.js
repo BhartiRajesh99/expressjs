711202869db291fdd4ea43b327e97c9a4316c740
RAJ <raku81453@gmail.com>
2025-04-02 02:21:52 +0530

    2nd commit

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,8 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger";
+import morgan from "morgan";
 
 const app = express();
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 const port = process.env.PORT || 4000;
 
 const teas = [];
